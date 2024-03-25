84a08b05324a161c71f8943d85a52c3e40857a2b
Pedro Ilidio <ilidio@usp.br>
2024-03-25 18:58:42 +0000

    fix bib and figures

---
+++ convert_dates.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -e
+
+IN=$1
+OUT=$(dirname $IN)/parsed_$(basename $IN)
+
+convert_date () {
+    local input_line=$1
+
+    if [[ $input_line == *"urldate"* ]] ; then
+        # Convert 2010-02-11 --> 11 Feb. 2010
+        local input_date=$(echo $input_line | sed "s/.*\([12][0-9]\{3\}-[01][0-9]-[0-3][0-9]\).*/\\1/")
+        local output_date=$(date -d $input_date "+%d %b. %Y")
+        # Convert 01 May. 2019 --> 01 May 2019
+        output_date=$(echo $output_date | sed "s/May./May/")
+        output_line=$(echo $input_line | sed "s/${input_date}/${output_date}/g")
+        echo $output_line
+    elif [[ $input_line == *"organization= "* ]] ; then
+        echo $input_line | sed "s/.*organization= \(.*\)}/organization = {\\1}/"
+    else
+        echo $input_line
+    fi
+    
+
+}
+# sed -r "s/([12][0-9]{3}-[01][0-9]-[0-3][0-9])/$(date -d \\1 '+%d %b. %Y')/g" $1 > $OUT
+# export -f convert_date
+# xargs -a $IN -I{} bash -c 'convert_date "{}"'
+
+while IFS= read -r line ; do
+    convert_date "$line"
+done < $IN > $OUT
+
+echo Saved to $OUT
