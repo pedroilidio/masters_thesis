7d3ee8db08010b6d43c041545a94935b692f39c5
Pedro Ilidio <ilidio@usp.br>
2024-04-01 20:59:40 +0000

    bibliography abnt review 2

--- parse_zotero_bibtex.sh
+++ parse_zotero_bibtex.sh
@@ -19,6 +19,8 @@ convert_date () {
         echo $input_line | sed "s/.*organization= \(.*\)}/organization = {\\1}/"
     elif [[ $input_line == "  title ="* ]] ; then
         echo $input_line | sed "s/title = {\(.*\): \(.*\)},/title = {\\1},\nsubtitle = {\\2},/"
+    elif [[ $input_line == "  booktitle ="* ]] ; then
+        echo $input_line | sed "s/booktitle = {\(.*\): \(.*\)},/booktitle = {\\1},\nbooksubtitle = {\\2},/"
     else
         echo $input_line
     fi
